954d2b69998c6af67ea494f812f194114d77c963
ahmadsss <ahmad@alsane.info>
2025-06-01 12:21:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -108,13 +108,11 @@ if question:
 
         with st.expander("📜 عرض المواد القانونية المسترجعة"):
             for obj in articles:
-                if obj.properties.get("article_title") == "LAW METADATA":
-                    continue  # ❌ لا تعرض هذا النوع
-        st.markdown(
+                st.markdown(
             f"<div style='direction: rtl; text-align: right;'><b>المادة {obj.properties.get('article_number')}</b> - {obj.properties.get('article_title')}</div>",
-            unsafe_allow_html=True
-        )
-        st.markdown(
+                unsafe_allow_html=True
+            )
+                st.markdown(
             f"<div style='direction: rtl; text-align: right; background-color: #012348; border-radius: 8px; padding: 8px; margin-bottom: 10px;'>{obj.properties.get('text').replace(chr(10), '<br>')}</div>",
-            unsafe_allow_html=True
-        )
+                unsafe_allow_html=True
+            )
